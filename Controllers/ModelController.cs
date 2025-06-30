07a6493ff1237e36166429e71246e6a68ea7325c
Kiran Kumar <kumar603>
2025-06-30 18:24:28 +0530

    SprintII-: Implemented initial structure for Model Forms Management

---
+++ Controllers/ModelController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LLM_Interaction.Data;
+using LLM_Interaction.Models;
+
+namespace LLM_Interaction.Controllers
+{
+    public class ModelController : Controller
+    {
+        private LLMDbContext db = new LLMDbContext();
+
+        // GET: Models
+        public async Task<ActionResult> Index()
+        {
+            var models = db.Models.Include(m => m.Provider);
+            return View(await models.ToListAsync());
+        }
+
+        // GET: Models/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Model model = await db.Models.FindAsync(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // GET: Models/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name");
+            return View();
+        }
+
+        // POST: Models/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,ProviderId")] Model model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Models.Add(model);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name", model.ProviderId);
+            return View(model);
+        }
+
+        // GET: Models/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Model model = await db.Models.FindAsync(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name", model.ProviderId);
+            return View(model);
+        }
+
+        // POST: Models/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,ProviderId")] Model model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(model).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name", model.ProviderId);
+            return View(model);
+        }
+
+        // GET: Models/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Model model = await db.Models.FindAsync(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // POST: Models/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Model model = await db.Models.FindAsync(id);
+            db.Models.Remove(model);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
